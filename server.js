6fb0802ed84544fa6f03cfc85296f5f33d9d6ff0
Mauricio Degregori <degregori2@gmail.com>
2019-12-13 10:12:58 -0600

    finished up setting up initial files + server sanity check passed

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+// const RecipeRouter = require('./recipes/recipe-router');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Sprint Challenge Sanity Check</h2>`);
+  });
+
+
+server.use(express.json());
+// server.use('/api/recipes', RecipeRouter);
+
+module.exports = server;
